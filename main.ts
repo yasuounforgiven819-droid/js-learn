af829ff6866646a8600f0d5d2670546daff04e20
Mayank Srivastava <mayanksrivastava@Mayanks-MacBook-Pro.local>
2026-02-14 12:43:17 +0530

    Testing

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ interface objType {
   name: string;
   age: number;
   address: string;
+  sex: string;
 }
 
 const name: string = "Mayank";
@@ -10,10 +11,13 @@ const age: number = 34;
 
 const address = "India";
 
+const sex = "Male";
+
 const object: objType = {
   name: name,
   age: age,
   address: address,
+  sex: sex,
 };
 
 console.log({ object: object });
